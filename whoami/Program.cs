873d2af4e19047b4c9dd367a559fe50f4c28b1f0
kursad.koc <kursadkoc@live.com>
2025-03-04 20:35:08 +0300

    initial commit

---
+++ whoami/Program.cs
@@ -0,0 +1,23 @@
+var builder = WebApplication.CreateBuilder(args);
+
+var app = builder.Build();
+
+app.MapGet("health", () => Results.Ok());
+
+app.MapGet("/", async (HttpContext context, IHostEnvironment environment) =>
+{
+    string message = $"""
+                     machine name: {Environment.MachineName}
+                     environment name: {environment.EnvironmentName} 
+                     trace id: {context.Request.HttpContext.TraceIdentifier} 
+                     protocol: {context.Request.Protocol} 
+                     method: {context.Request.Method}
+                     contentRoot: {environment.ContentRootPath}
+                     path: {context.Request.Path}
+                     os version: {Environment.OSVersion.VersionString}
+                     processors: {Environment.ProcessorCount}
+                     """;
+    await context.Response.WriteAsync(message);
+});
+
+app.Run();
