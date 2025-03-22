15ef2b6179fb34b86f77329d84a127d247188c03
kursad.koc <kursadkoc@live.com>
2025-03-23 00:28:44 +0300

    clean solution

--- whoami/Program.cs
+++ whoami/Program.cs
@@ -1,5 +1,3 @@
-using Microsoft.AspNetCore.Mvc;
-
 var builder = WebApplication.CreateBuilder(args);
 
 var app = builder.Build();
@@ -21,15 +19,6 @@ app.MapGet("/", async (HttpContext context, IHostEnvironment environment) =>
                       """;
     
     await context.Response.WriteAsync(message);
-});
-
-app.MapGet("config", ([FromServices] IConfiguration configuration) =>
-{
-    string header = configuration.GetValue<string>("SiteSettings:Header");
-    string title = configuration.GetValue<string>("SiteSettings:Title");
-    string color = configuration.GetValue<string>("SiteSettings:BackgroundColor");
-    
-    return Results.Ok(new { header = header, title = title, color = color });
 });
 
 app.Run();
