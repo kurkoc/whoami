b31d72c071bcf51caa170bcb9f09df8635c9a724
kursad.koc <kursadkoc@live.com>
2025-03-23 00:23:42 +0300

    some changes

--- whoami/Program.cs
+++ whoami/Program.cs
@@ -1,3 +1,5 @@
+using Microsoft.AspNetCore.Mvc;
+
 var builder = WebApplication.CreateBuilder(args);
 
 var app = builder.Build();
@@ -7,16 +9,17 @@ app.MapGet("health", () => Results.Ok());
 app.MapGet("/", async (HttpContext context, IHostEnvironment environment) =>
 {
     string message = $"""
-                     machine name: {Environment.MachineName}
-                     environment name: {environment.EnvironmentName} 
-                     trace id: {context.Request.HttpContext.TraceIdentifier} 
-                     protocol: {context.Request.Protocol} 
-                     method: {context.Request.Method}
-                     contentRoot: {environment.ContentRootPath}
-                     path: {context.Request.Path}
-                     os version: {Environment.OSVersion.VersionString}
-                     processors: {Environment.ProcessorCount}
-                     """;
+                      machine name: {Environment.MachineName}
+                      environment name: {environment.EnvironmentName} 
+                      trace id: {context.Request.HttpContext.TraceIdentifier} 
+                      protocol: {context.Request.Protocol} 
+                      method: {context.Request.Method}
+                      contentRoot: {environment.ContentRootPath}
+                      path: {context.Request.Path}
+                      os version: {Environment.OSVersion.VersionString}
+                      processors: {Environment.ProcessorCount}
+                      """;
+    
     await context.Response.WriteAsync(message);
 });
 
